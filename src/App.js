123f7de7e138002e2eefd8c50b853155db5f31d1
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-07-01 11:59:38 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import HookJsonFetch from "./components/HookJsonFetch";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="ui container">
+        <div className="ui segments">
+          <HookJsonFetch url={process.env.REACT_APP_DATA_URL} />
+          <HookJsonFetch url={process.env.REACT_APP_ERROR_URL} />
+          <HookJsonFetch url={process.env.REACT_APP_LOADING_URL} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
